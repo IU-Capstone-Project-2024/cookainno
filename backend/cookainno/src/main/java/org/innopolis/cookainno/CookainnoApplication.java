f832f69e8e9be83d7b39654ddfd9ef2742b27aa5
vladislav77777 <vladoss7700@mail.ru>
2024-06-09 14:25:42 +0300

    init

---
+++ backend/cookainno/src/main/java/org/innopolis/cookainno/CookainnoApplication.java
@@ -0,0 +1,13 @@
+package org.innopolis.cookainno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookainnoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookainnoApplication.class, args);
+    }
+
+}
---
+++ backend/cookainno/src/test/java/org/innopolis/cookainno/CookainnoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.innopolis.cookainno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookainnoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
