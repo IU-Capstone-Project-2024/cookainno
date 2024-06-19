f09c80ec835104cc203acaf14cd91c132b2a64c6
Polina Pushkareva <p.pushkareva@innopolis.university>
2024-06-19 14:31:58 +0300

    fastapi test code

--- mistralAPI/mistral.py
+++ mistralAPI/mistral.py
@@ -3,20 +3,29 @@ from mistralai.client import MistralClient
 from mistralai.models.chat_completion import ChatMessage
 import config
 
+
 # TODO: написать функцию, котрая принимает список ингридиентов и возвращает рецепт (словарь)
+# TODO: генерируем 5 названий рецептов по ингредиентам, генерируем подробный рецепт по названию
 
 # api_key = os.environ["MISTRAL_API_KEY"]
-api_key = config.api_key
-model = config.model
+
+def generate_recipes(ingredients):
+    api_key = config.api_key
+    model = config.model
+
+    client = MistralClient(api_key=api_key)
+
+    chat_response = client.chat(
+        model=model,
+        messages=[ChatMessage(role="user",
+                              content=f"Propose recipes in which {ingredients} can be used. You need to provide 1 "
+                                      f"variants of meals. It is sufficient just to show the name, ingredients list "
+                                      f"for, and instruction for the preparation for each meal. Compose a response in "
+                                      f"json format with fields: 'name', 'ingredients', and 'instruction'")]
+    )
+
+    return chat_response.choices[0].message.content
+
 
 request = input("Enter the ingredients: ")
-ingredients = request.split(",")
-
-client = MistralClient(api_key=api_key)
-
-chat_response = client.chat(
-    model=model,
-    messages=[ChatMessage(role="user", content=f"Propose a recepies in which {request} can be used. You need to provide 3 variants of meals. It is sufficient just to show the name and ingredients list for each meal.")]
-)
-
-print(chat_response.choices[0].message.content)
+print(generate_recipes(request))
