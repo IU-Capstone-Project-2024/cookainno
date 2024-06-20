2c7223ad5d860ddb256a0f7fcc4598bad2a0a91c
Polina Pushkareva <p.pushkareva@innopolis.university>
2024-06-20 12:16:20 +0300

    Modified fastapi code

--- mistralAPI/main.py
+++ mistralAPI/main.py
@@ -10,16 +10,13 @@ def read_root():
     return {"Hello": "World"}
 
 
-# @app.get("/ingredients/{items}")
-# def read_item(items: List[str]):
-#     return mistral.generate_recipes(items)
-#     # return {"item_id": item_id, "q": q}
-
 @app.get("/ingredients/")
 def read_ingredient(items: List[str] = Query(None)):
+    # Usage example: http://127.0.0.1:8000/ingredients/?items=apple&items=milk&items=strawberry%jam
     return mistral.generate_recipes(items)
 
 
 @app.get("/advice/{query}")
 def read_advice(query: str):
+    # Usage example: http://127.0.0.1:8000/advice/deficit
     return mistral.daily_advice(query)
--- mistralAPI/mistral.py
+++ mistralAPI/mistral.py
@@ -3,9 +3,6 @@ from mistralai.client import MistralClient
 from mistralai.models.chat_completion import ChatMessage
 import config
 
-# TODO: написать функцию, котрая принимает список ингридиентов и возвращает рецепт (словарь)
-# TODO: генерируем 5 названий рецептов по ингредиентам, генерируем подробный рецепт по названию
-
 # api_key = os.environ["MISTRAL_API_KEY"]
 
 api_key = config.api_key
@@ -14,20 +11,36 @@ client = MistralClient(api_key=api_key)
 
 
 def generate_recipes(ingredients):
+    """
+    Generate recipes based on a list of ingredients.
+
+    Args:
+        ingredients (list): A list of ingredients.
+
+    Returns:
+        str: A JSON string containing the generated recipes.
+    """
     chat_response = client.chat(
         model=model,
         messages=[ChatMessage(role="user",
-                              content=f"Propose recipes in which {ingredients} can be used. You need to provide 1 "
+                              content=f"Propose recipes in which {ingredients} can be used. You need to provide 5 "
                                       f"variants of meals. It is sufficient just to show the name, ingredients list "
                                       f"for, and instruction for the preparation for each meal. Compose a response in "
                                       f"json format with fields: 'name', 'ingredients', and 'instruction'.")]
     )
-
     return chat_response.choices[0].message.content
 
 
-# plan: normal consumption/deficit/surplus
 def daily_advice(plan):
+    """
+    Generate daily advice based on a calorie plan.
+
+    Args:
+        plan (str): The calorie plan (normal consumption, deficit, or surplus).
+
+    Returns:
+        str: A short one-sentence daily advice.
+    """
     chat_response = client.chat(
         model=model,
         messages=[ChatMessage(role="user",
